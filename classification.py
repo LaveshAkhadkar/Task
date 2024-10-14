f258e10445810fa63f7b97eb265ce9b05eab39c5
user-Zenxy <88881276+user-Zenxy@users.noreply.github.com>
2024-10-15 02:44:15 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -2,7 +2,36 @@ from transformers import BertTokenizer
 import tensorflow as tf
 from huggingface_hub import from_pretrained_keras
 
-loaded_model.built = True
+from transformers import TFBertModel
+import tensorflow as tf
+from huggingface_hub import KerasModelHubMixin
+
+class HierarchicalBERTModel(tf.keras.Model, KerasModelHubMixin):
+    def __init__(self, num_cat1=6, num_cat2=21, bert_model_name='bert-base-uncased'):
+        super(HierarchicalBERTModel, self).__init__()
+        self.bert = TFBertModel.from_pretrained(bert_model_name)
+        self.cat1_classifier = tf.keras.layers.Dense(num_cat1, activation='softmax')
+        self.cat2_classifier = tf.keras.layers.Dense(num_cat2, activation='softmax')
+
+    def call(self, inputs):
+        # Allow inputs in both tuple and dict format
+        if isinstance(inputs, dict):
+            input_ids = inputs['input_ids']
+            attention_mask = inputs['attention_mask']
+        else:
+            input_ids, attention_mask = inputs
+        bert_output = self.bert.bert(input_ids=input_ids, attention_mask=attention_mask)
+        cls_token_output = bert_output.pooler_output
+
+        cat1_logits = self.cat1_classifier(cls_token_output)
+        cat2_logits = self.cat2_classifier(cls_token_output)
+
+        return cat1_logits, cat2_logits
+
+
+
+loaded_model = HierarchicalBERTModel()
+
 loaded_model = from_pretrained_keras("Lavesh-Akhadkar/hierarchical-bert-model")
 
 dummy_input_ids = tf.zeros((1, 128), dtype=tf.int32)
