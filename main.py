d2937d555eae2133a29cf8e7e5279a108a83faec
Lavesh <88881276+LaveshAkhadkar@users.noreply.github.com>
2024-10-26 00:04:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
-from expansion import generate_expanded_query
+from expansion import generate_expanded_query, clear_history
 from classification import predict_with_loaded_model
 
 load_dotenv()
 
 st.title("Chatbot with BERT Classification and BART Expansion")
 st.write("Classification and expansion start after 2nd message by the user")
+
 if "history" not in st.session_state:
     st.session_state.history = []
 
@@ -19,43 +20,63 @@ user_query = st.text_input("You:", key="input")
 
 genai.configure(api_key=os.getenv("gemini"))
 
+
 @st.cache_resource
 def get_model():
-    return genai.GenerativeModel("gemini-1.5-flash", system_instruction="Always answer in short 2-3 sentences.")
+    return genai.GenerativeModel(
+        "gemini-1.5-flash", system_instruction="Always answer in short 2-3 sentences."
+    )
+
 
 model = get_model()
 chat = model.start_chat(history=st.session_state.ghistory)
 
-#if user clicks send or presses enter with user query in input
 if st.button("Send") or user_query:
     if user_query:
-
-        if len(st.session_state.ghistory) > 1:
-            last_20_messages = st.session_state.ghistory[-20:]
-            flattened_chat_history = " ".join([entry["parts"] for entry in last_20_messages])
-            summary = model.generate_content("Summarize this: " + flattened_chat_history)
-
-        else:
-            summary = "Not available"
-
-        
         response = chat.send_message(user_query).text
+
         if len(st.session_state.ghistory) > 1:
-            expanded_query = generate_expanded_query(user_query, summary)
+            expanded_query = generate_expanded_query(user_query)
+
+            print(expanded_query)
 
             cat1_pred, cat2_pred = predict_with_loaded_model(expanded_query)
-            topic1 = ['Entertainment','Technology','Politics','Health','Sports','General']
-            topic2 = ['Movies', 'Software Development', 'Global', 'Music', 'Nutrition', 
-                'TV Shows', 'Fitness', 'Mental Health', 'Cybersecurity', 'UK', 
-                'Diseases', 'Blockchain', 'USA', 'AI', 'Cricket', 'Celebrities', 
-                'Football', 'Basketball', 'India', 'Tennis', 'General']
+            topic1 = [
+                "Entertainment",
+                "Technology",
+                "Politics",
+                "Health",
+                "Sports",
+                "General",
+            ]
+            topic2 = [
+                "Movies",
+                "Software Development",
+                "Global",
+                "Music",
+                "Nutrition",
+                "TV Shows",
+                "Fitness",
+                "Mental Health",
+                "Cybersecurity",
+                "UK",
+                "Diseases",
+                "Blockchain",
+                "USA",
+                "AI",
+                "Cricket",
+                "Celebrities",
+                "Football",
+                "Basketball",
+                "India",
+                "Tennis",
+                "General",
+            ]
             classification = f"{topic1[cat1_pred]}-{topic2[cat2_pred]}"
         else:
             expanded_query = ""
             classification = ""
 
-
-        # Append user and bot responses to history
         st.session_state.history.append(
             {
                 "speaker": "User",
@@ -66,13 +87,9 @@ if st.button("Send") or user_query:
         )
         st.session_state.history.append({"speaker": "Bot", "text": response})
 
-        st.session_state.ghistory.append(
-            {"role": "user", "parts": f"{user_query}"},
-        )
-        st.session_state.ghistory.append({"role": "model", "parts": f"{response}"})
+        st.session_state.ghistory.append({"role": "user", "parts": user_query})
+        st.session_state.ghistory.append({"role": "model", "parts": response})
 
-
-        # Display the entire conversation
         for entry in st.session_state.history:
             if entry["speaker"] == "User":
                 st.write(
@@ -84,5 +101,6 @@ if st.button("Send") or user_query:
 if st.button("Clear Conversation"):
     st.session_state.history = []
     st.session_state.ghistory = []
-
+    clear_history()
     st.write("Conversation history cleared!")
+
